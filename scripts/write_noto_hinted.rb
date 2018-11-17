32c72a82483600e2d4ffe7ee916d336c63cda2cf
marguerite <i@marguerite.su>
2018-11-17 13:23:10 +0800

    no need to generate for eg Noto Sans Black which is a different weight for Noto Sans

--- scripts/write_noto_hinted.rb
+++ scripts/write_noto_hinted.rb
@@ -1,39 +1,50 @@
 #!/usr/bin/env ruby
 
-file = open("noto-hinting.txt", "r:UTF-8")
+file = open('noto-hinting.txt', 'r:UTF-8')
 
 fonts = []
-tmp = ""
-hinted = []
-non_hinted = []
+tmp = ''
+hinted = open('noto-hinted.txt', 'w:UTF-8')
+non_hinted = open('noto-non-hinted.txt', 'w:UTF-8')
 
 file.each_line do |l|
-	next if l.strip.end_with?(".ttf")
-	tmp += l if l.strip.index("_")
-	if l.strip.end_with?("true") || l.strip.end_with?("false")
-	  unless tmp.empty?
-            tmp += l
-	    fonts << tmp
-	    tmp = ""
-	  end  
-	end
+  next if l.strip.end_with?('.ttf')
+  tmp += l if l.strip.index('_')
+  next unless l.strip.end_with?('true', 'false')
+  next if tmp.empty?
+  tmp += l
+  fonts << tmp
+  tmp = ''
 end
 
 file.close
 
-fonts.each do |f|
+fonts.map! do |f|
   a = f.split("\n")
-  if a[-1].strip.end_with?("true")
-    a[0..-2].each {|i| hinted << i}
+  s = a.size
+  ui = a.select { |m| m.end_with?('_UI') }
+  if s < 3
+    a
+  elsif !ui.empty?
+    [ui[0], a[-1]]
   else
-    a[0..-2].each {|i| non_hinted << i}
+    idx = a[0].split('_').size - 1
+    r = Regexp.new('^' + '[[:alnum:]]+_' * idx + '[[:alnum:]]+')
+    a.each_with_index do |i, j|
+      next if j.zero? || j == s - 1
+      a[j] = r.match(i)[0]
+    end.uniq!
   end
 end
 
-h = open("noto-hinted.txt", "w:UTF-8")
-hinted.each {|i| h.write i + "\n"}
-h.close
+fonts.each do |i|
+  next if i[0].index('Emoji')
+  if i[1].strip.end_with?('true')
+    hinted.write i[0] + "\n"
+  else
+    non_hinted.write i[0] + "\n"
+  end
+end
 
-nh = open("noto-non-hinted.txt", "w:UTF-8")
-non_hinted.each {|i| nh.write i + "\n"}
-nh.close
+hinted.close
+non_hinted.close
